8916a90764d1d60a305a6e7fd413f37b48dc231b
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-22 16:30:01 +0100

    Serer

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
+#!/usr/bin/pup
 # install puppet-lint -v 2.5.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/pip3 install flask -v 2.1.0',
+package { 'flask':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
