589a43a09d912f6470c15133c97c647e917675b5
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-22 17:32:22 +0100

    Server

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # install flask -v 2.1.0
 
-exec {'pip3 install flask':
-require => Exec['python-installed'],
+exec {'pip3 install flask':,
 command => '/usr/bin/pip3 install flask==2.1.0'
 }
 
-exec {'python-installed':
-command => '/usr/bin/which pythonn3'
-}
