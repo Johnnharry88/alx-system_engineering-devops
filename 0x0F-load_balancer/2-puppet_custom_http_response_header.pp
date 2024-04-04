2e176a7692ab8b28e2ded05905aaea4d2176306d
Johnnharry88 <harryjohnwhye@gmail.com>
2024-04-04 08:41:03 +0100

    Server Config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ exec {'update':
   ensure => 'present',
 }
 -> file_line {'http_header':
-  path  => '/etc/nginx/nginx.cnf',
+  path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${HOSTNAME}\";",
 }
