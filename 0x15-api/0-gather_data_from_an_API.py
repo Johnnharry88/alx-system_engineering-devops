2333d93f2da3de61274505c543495b606958fcfd
Johnnharry88 <harryjohnwhye@gmail.com>
2024-04-22 12:20:49 +0100

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Accessing REST API for empplyee's to do list"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    id_employee = sys.argv[1]
+    access_url = "https://jsonplaceholder.typicode.com/users"
+    url = access_url + '/' + id_employee
+
+    response = requests.get(url)
+    employee_name = response.json().get('name')
+
+    url_toddo = url + '/todos'
+    response = requests.get(url_toddo)
+    work = response.json()
+    done = 0
+    task_done = []
+    for task in work:
+        if task.get('completed'):
+            task_done.append(task)
+            done = done + 1
+    print("Employee {} is done wih task ({}/{}):"
+          .format(employee_name, done, len(work)))
+    for task in task_done:
+        print("\t {}".format(task.get('title')))
