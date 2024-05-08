71471b2d3435f6ec879fc759f5fb9c841c7f7f89
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-08 05:20:26 +0100

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,23 +8,23 @@ from requests import get
 
 
 def top_ten(subreddit):
-        """Query the Reddit API and prints the title of the fitst
-        10 hot posts listed in a given reddict"""
+    """Query the Reddit API and prints the title of the fitst
+    10 hot posts listed in a given reddict"""
 
-        if subreddit is None or not isinstance(subreddit, str):
-            print ("None")
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
 
-            def_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-            param = {'limit': 10}
-            url_x = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    def_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    param = {'limit': 10}
+    url_x = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-            response = get(url_x, headers=def_agent, params=param)
-            res = response.jsnn()
+    response = get(url_x, headers=def_agent, params=param)
+    res = response.jsnn()
 
-            try:
-                data = res.get('data').get('children')
+    try:
+        data = res.get('data').get('children')
 
-                for x in data:
-                    print(x.get('data').get('title'))
-            except Exception:
-                print("None")
+        for x in data:
+            print(x.get('data').get('title'))
+    except Exception:
+        print("None")
