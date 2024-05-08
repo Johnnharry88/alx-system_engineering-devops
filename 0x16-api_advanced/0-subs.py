cbd789c86f1145327cd71f5215abc9909a3676c3
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-08 07:12:39 +0100

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    def_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=def_agent)
+    response = get(url, headers=user_agent)
     res = response.json()
 
     try:
