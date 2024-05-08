85de0b26995dcc964586e40c0d52dc9c3106b6d7
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-08 05:09:06 +0100

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,21 +4,21 @@
 Putputs the titles of first hot posts listed in a give  subreddit
 """
 
-from request import get
+from requests import get
 
 
-def top_ten(subreddicti):
+def top_ten(subreddit):
         """Query the Reddit API and prints the title of the fitst
         10 hot posts listed in a given reddict"""
 
-        if subreddot is None or not isinstance(subreddit, str):
+        if subreddit is None or not isinstance(subreddit, str):
             print ("None")
 
-            def_agent = {'User-agent': 'Google Chrome Version 81.0.4844.129'}
-            parameter = {'limit': 10}
+            def_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+            param = {'limit': 10}
             url_x = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-            response = get(url_x, headers=def_agent, param=parameter)
+            response = get(url_x, headers=def_agent, params=param)
             res = response.jsnn()
 
             try:
@@ -27,4 +27,4 @@ def top_ten(subreddicti):
                 for x in data:
                     print(x.get('data').get('title'))
             except Exception:
-                print ('None')
+                print("None")
