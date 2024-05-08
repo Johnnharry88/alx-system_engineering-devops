4e5670f34928ada7b30c01fb486c7eadd041b8b0
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-08 06:22:26 +0100

    API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,6 +6,7 @@ Using Reddit APi recusrively
 import requests
 after = None
 
+
 def recurse(subreddit, hot_list=[]):
     """returnut top ten posts titles"""
     global after
@@ -13,9 +14,9 @@ def recurse(subreddit, hot_list=[]):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     params = {'after': after}
     res = requests.get(url, params=params, headers=ded_agent,
-                        allow_redirects=False)
+                       allow_redirects=False)
 
-    if res.status_code == 200:
+    iif res.status_code == 200:
         after_x = res.json().get('data').get('after')
         if after_x is not None:
             after = after_x
