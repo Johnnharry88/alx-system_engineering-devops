af49567165913dd13ef74d9679aadd6a8c244cff
Johnharry88 <harryjohnwhye@gmail.com>
2024-05-08 04:29:18 +0100

    API Advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Gat the number of subscribers for a given subreddit"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Quieries API 
+    Return: Number of subscribes (not active users, total subscriber)
+    for a give subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    agent_def = {'User-agent': 'Google Chrome Version 81.0.4844.129'}
+    url_x = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url_x, headers=agent_def)
+    res = response.json()
+    try:
+        return res.get('data').get('subscribers')
+    except Exception:
+        return 0
